c718e2f01bf52b778fc9ca8e512afeeaf627f257
yonatan braude <yonatan.braude@saymediagroup.com>
2013-08-25 01:11:41 +0300

    added package.json to repository and creator script

--- expresscoffee.sh
+++ expresscoffee.sh
@@ -1,8 +1,12 @@
 express $1  --css less 
 cd $1
+echo `pwd`
+
+
 mkdir ./build
 mv ./* ./build/
 mkdir -p ./src/routes
+
 
 js2coffee ./build/app.js > ./src/app.coffee
 js2coffee ./build/routes/index.js > ./src/routes/index.coffee
@@ -10,4 +14,18 @@ js2coffee ./build/routes/user.js > ./src/routes/user.coffee
 
 rm -rf ./build/routes
 rm ./build/app.js
+
+touch ./package.json
+
+echo $'{
+  		\"name\": \"express-yourself\", 
+  		\"version\": \"0.0.0\",
+  		\"description\": \"express (your self) with coffee",
+  		\"dependencies\": {
+    		\"grunt\": \"~0.4.1\",
+    		\"grunt-contrib-watch\": \"~0.5.2\",
+    		\"haml\": \"~0.4.3\"
+  		},
+  		\"devDependencies\": {}
+  	}' > ./package.json
 
